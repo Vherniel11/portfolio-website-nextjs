22daee4dbf2dc53c8453821a9616815d82d04edf
Vherniel Lebis <vherniel11@gmail.com>
2022-08-31 21:08:56 +0800

    Remove Indent from Menu

--- components/header/menu/Menu.js
+++ components/header/menu/Menu.js
@@ -8,10 +8,8 @@ const Menu = ({ toggle }) => {
     return (
         <MenuSection>
             <Container>
-                <Indent>
-                    <MenuNav toggle={toggle} />
-                    <MenuInfo />
-                </Indent>
+                <MenuNav toggle={toggle} />
+                <MenuInfo />
             </Container>
         </MenuSection>
     );
@@ -26,15 +24,6 @@ var MenuSection = styled.div`
         0px 10.7px 10.7px -6px rgba(0, 0, 0, 0.03),
         0px 20.1px 20.1px -6px rgba(0, 0, 0, 0.024),
         0px 48px 48px -6px rgba(0, 0, 0, 0.017);
-`;
-
-var Indent = styled.div`
-    margin-left: 2em;
-    margin-right: 1em;
-
-    @media (min-width: 40rem) {
-        margin: 0;
-    }
 `;
 
 export default Menu;
