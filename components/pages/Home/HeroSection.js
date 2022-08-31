7b9b6719a858885ad580099401163b57642ca79f
Vherniel Lebis <vherniel11@gmail.com>
2022-08-31 19:00:30 +0800

    Add tech stack icons

--- components/pages/Home/HeroSection.js
+++ components/pages/Home/HeroSection.js
@@ -1,6 +1,15 @@
 import Container from '../../shared/container/Container.styled';
 import styled from 'styled-components';
 
+// React Icons
+import { IconContext } from 'react-icons';
+import { IoLogoReact } from 'react-icons/io5';
+import { IoLogoNodejs } from 'react-icons/io5';
+import { IoLogoSass } from 'react-icons/io5';
+import { SiSvelte } from 'react-icons/si';
+import { SiTypescript } from 'react-icons/si';
+import { SiTailwindcss } from 'react-icons/si';
+
 const HeroSection = () => {
     return (
         <section>
@@ -10,6 +19,65 @@ const HeroSection = () => {
                     deliver dynamic, user-friendly, and effective websites with
                     JavaScript technologies.
                 </Headline>
+                <div>
+                    <p>Web technologies I use</p>
+                    <IconsFlex>
+                        <IconContext.Provider
+                            value={{
+                                color: '#5ED3F3',
+                                size: '1.5em',
+                            }}>
+                            <Icon>
+                                <IoLogoReact />
+                            </Icon>
+                        </IconContext.Provider>
+                        <IconContext.Provider
+                            value={{
+                                color: '#F73C00',
+                                size: '1.5em',
+                            }}>
+                            <Icon>
+                                <SiSvelte />
+                            </Icon>
+                        </IconContext.Provider>
+                        <IconContext.Provider
+                            value={{
+                                color: '#68A063',
+                                size: '1.6em',
+                            }}>
+                            <Icon>
+                                <IoLogoNodejs />
+                            </Icon>
+                        </IconContext.Provider>
+                        <IconContext.Provider
+                            value={{
+                                color: '#2F74C0',
+                                size: '1.5em',
+                            }}>
+                            <Icon>
+                                <SiTypescript />
+                            </Icon>
+                        </IconContext.Provider>
+                        <IconContext.Provider
+                            value={{
+                                color: '#38BDF8',
+                                size: '1.5em',
+                            }}>
+                            <Icon>
+                                <SiTailwindcss />
+                            </Icon>
+                        </IconContext.Provider>
+                        <IconContext.Provider
+                            value={{
+                                color: '#C45F92',
+                                size: '1.5em',
+                            }}>
+                            <Icon>
+                                <IoLogoSass />
+                            </Icon>
+                        </IconContext.Provider>
+                    </IconsFlex>
+                </div>
             </Container>
         </section>
     );
@@ -26,6 +94,14 @@ var Headline = styled.h1`
         font-size: 3rem;
         margin-top: 5rem;
     }
+`;
+
+var IconsFlex = styled.div`
+    display: flex;
+`;
+
+var Icon = styled.div`
+    margin-right: 1em;
 `;
 
 export default HeroSection;
